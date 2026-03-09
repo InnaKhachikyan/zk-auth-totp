1080a7e088d1012fbeb9cd53c66d807e24df79be
Inna Khachikyan <khachikyaninna@gmail.com>
2026-03-10 00:25:03 +0400

    username logic added

--- client/src/auth.rs
+++ client/src/auth.rs
@@ -1,6 +1,16 @@
 use rpassword::read_password;
 use std::io::{self,Write};
 
+pub fn read_username() -> String {
+    let mut username = String::new();
+    print!("Enter Username: ");
+    io::stdout().flush().expect("Failed to flush stdout");
+    io::stdin().read_line(&mut username).expect("Failed to read username");
+    let username = username.trim();
+    let username = username.to_string();
+    username
+}
+
 pub fn read_user_password() -> String {
     print!("Enter Password: ");
     io::stdout().flush().unwrap();
--- client/src/main.rs
+++ client/src/main.rs
@@ -4,19 +4,25 @@ use shared::crypto::schnorr::keypair_gen;
 use shared::crypto::kdf::salt_gen;
 use shared::crypto::kdf::derive_key_from_password;
 use auth::read_user_password;
+use auth::read_username;
 
 fn main() {
+
+    let username = read_username();
+    println!("Username stored: {} ", username);
+
+    let password = read_user_password();
+    println!("Password stored: {:?} ", password);
+    
+    let salt = salt_gen();
+    println!("Salt is: {:?}", salt);
+    
+    let key = derive_key_from_password(&password, &salt).expect("KDF failed");
+    println!("Key is: {:?} ", key);
+    
     let (x, y) = keypair_gen();
 
     println!("Secret x: {:?}", x.to_bytes());
     println!("Public Y: {:?}", y.compress().to_bytes());
 
-    let salt = salt_gen();
-
-    println!("Salt is: {:?}", salt);
-    
-    let password = read_user_password();
-    println!("Password stored: {:?} ", password);
-    let key = derive_key_from_password(&password, &salt).expect("KDF failed");
-    println!("Key is: {:?} ", key);
 }
