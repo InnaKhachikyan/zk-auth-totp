b0f3f8bacdd31e821fa40e58431a6175307b2095
Inna Khachikyan <khachikyaninna@gmail.com>
2026-03-15 23:36:57 +0400

    client/server message types organized

--- shared/src/messages.rs
+++ shared/src/messages.rs
@@ -12,7 +12,41 @@ pub struct LoginStartRequest {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+pub struct LoginProofRequest {
+    //placeholder for schnorr proof of knowledge of secret x
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub enum ClientMessage {
     Register(RegisterRequest),
     LoginStart(LoginStartRequest),
+    LoginProof(LoginProofRequest),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum RegisterResponse {
+    Success,
+    Failure {message: String},
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum LoginStartResponse {
+    Success {
+        nonce: [u8; 16], 
+        pub_dh: [u8; 32],
+    },
+    Failure {message: String},
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum LoginResult {
+    Success,
+    Failure {message: String},
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum ServerMessage {
+    Register(RegisterResponse),
+    LoginStart(LoginStartResponse),
+    LoginResult(LoginResult),
 }
